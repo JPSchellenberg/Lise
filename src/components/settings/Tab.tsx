5d81e6526c0742b86dfb9b2b10c288aa14e44527
Lipp <Lipp@miDrum.net>
2016-10-23 15:19:14 +0200

    Add simple type checking for settings

--- src/components/settings/Tab.tsx
+++ src/components/settings/Tab.tsx
@@ -15,6 +15,7 @@ export default class Tab extends React.Component<IProps, IState> {
 		super(props);
 
 		this.handleChange = this.handleChange.bind(this);
+		this.toggle = this.toggle.bind(this);
 	}
 
 	handleChange(key: string, event) { 
@@ -28,22 +29,48 @@ export default class Tab extends React.Component<IProps, IState> {
 		this.props.changeSettings( assign({}, this.props.settings, newValue));
 	}
 
+	toggle(key: string) {
+		let newValue = {};
+		newValue[key] = !this.props.settings[key];
+		this.props.changeSettings( assign({}, this.props.settings, newValue ));
+	}
+
 	render() {
 
 		let settings = [];
 		for (let key in this.props.settings) {
-			// const type = this.props.settings[key].constructor.name ;
-			settings.push(<div 
-								key={key}
-								className="form-group">
-							<label htmlFor="inputEmail" className="control-label col-xs-2">{key}</label>
-							<div className="col-xs-10">
-								<input 
+			let type = this.props.settings[key] ? this.props.settings[key].constructor.name : typeof this.props.settings[key] ;
+			if (this.props.settings[key] == 'false' || this.props.settings[key] == 'true') { type = 'Boolean'; }
+
+			let Input;
+			switch(type) {
+
+
+				case 'Boolean':
+				case 'boolean':
+					Input = <input 
+								onChange={ () => this.toggle(key) }
+								type="checkbox"
+								checked={this.props.settings[key]}
+								className="checkbox"
+					/>
+				break;
+				case 'String':
+				default: 
+					Input = <input 
 								onChange={ (e) => this.handleChange(key, e) }
 								value={ this.props.settings[key] !== null ? this.props.settings[key] : ''}
 								type="text"
 								className="form-control"
 								></input>
+				break;
+			}
+			settings.push(<div 
+								key={key}
+								className="form-group">
+							<label htmlFor="inputEmail" className="control-label col-xs-2">{key}</label>
+							<div className="col-xs-10">
+							{ Input }
 							</div>
 						</div>);
 		}
