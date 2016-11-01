333aac29bc21bb9827c99133767430591b6ede32
Lipp <Lipp@miDrum.net>
2016-11-01 20:55:27 +0100

    Disable persistent storage

--- src/core/store.ts
+++ src/core/store.ts
@@ -4,33 +4,33 @@ import { throttle } from 'lodash';
 
 import rootReducer              from './reducer';
 
-export function loadState() {
-	try {
-		const serializedState = localStorage.getItem('state');
-		if (serializedState === null) {
-			return undefined;
-		}
-		return JSON.parse(serializedState);
-	} catch(err) { return undefined; }
-}
+// export function loadState() {
+// 	try {
+// 		const serializedState = localStorage.getItem('state');
+// 		if (serializedState === null) {
+// 			return undefined;
+// 		}
+// 		return JSON.parse(serializedState);
+// 	} catch(err) { return undefined; }
+// }
 
-export function saveState(state) {
-	try {
-		const serializedState = JSON.stringify(state);
-		localStorage.setItem('state', serializedState);
-	} catch (err) {
-		debugger;
-	}
-}
+// export function saveState(state) {
+// 	try {
+// 		const serializedState = JSON.stringify(state);
+// 		localStorage.setItem('state', serializedState);
+// 	} catch (err) {
+// 		debugger;
+// 	}
+// }
 
-const persistentState = process.env.NODE_ENV !== 'development' ? loadState() : undefined;
+const persistentState = /*process.env.NODE_ENV !== 'development' ? loadState() :*/ undefined;
 
 declare var window: any;
 
 const store = createStore<any>(rootReducer, persistentState, window.devToolsExtension && window.devToolsExtension());
 
-		store.subscribe(throttle( () => {
-			saveState( store.getState() );
-		}, 1000) );
+		// store.subscribe(throttle( () => {
+		// 	saveState( store.getState() );
+		// }, 1000) );
 
 export default store;
