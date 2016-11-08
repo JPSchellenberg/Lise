6a29614261168d62a355fb94389918a588c108ea
Lipp <Lipp@miDrum.net>
2016-11-08 21:26:57 +0100

    Add test-mode for server

--- server/boot/socket.ts
+++ server/boot/socket.ts
@@ -18,5 +18,11 @@ export default function(server) {
 	serialport.on('error', (error) => channel['serialport'].emit('error', error));
 
 	test.on('test', (test) => channel['test'].emit('test', test));
+
+	if (process.env.TEST) { 
+		setInterval(() => {
+            channel['serialport'].emit('data', "data "+JSON.stringify({"time":new Date().getTime(), "channel1": Math.floor((Math.random() * 1000) + 1), "channel2": Math.floor((Math.random() * 10) + 1) })); 
+		}, 1000);
+	}
 }
 
