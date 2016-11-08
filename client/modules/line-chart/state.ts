d8630088e4179a2b70a003103af1b9072e2c0dc0
Lipp <Lipp@miDrum.net>
2016-11-08 22:15:24 +0100

    Update default line-chart settings

--- client/modules/line-chart/state.ts
+++ client/modules/line-chart/state.ts
@@ -6,7 +6,7 @@ export default class LineChartState {
 				title: ''
 			},
 			XAxis: new AxisSettings('Time [s]'),
-			YAxis: new AxisSettings('Y', -10, 10)
+			YAxis: new AxisSettings('Spannung [mV]', -10, 10)
 		};
 	}
 
