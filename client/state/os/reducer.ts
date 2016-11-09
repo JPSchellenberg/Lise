a1f3e5c55c7467512f6306d46b46d4ae8de76bc3
Lipp <Lipp@miDrum.net>
2016-11-09 22:07:12 +0100

    Fix bug in os-api

--- client/state/os/reducer.ts
+++ client/state/os/reducer.ts
@@ -4,13 +4,11 @@ import {
 	OS_SET_OS
 } from '../action-types';
 
-export default function(state = {
-	os: {}
-}, action) {
+export default function(state = {}, action) {
 	switch (action.type) {
 
 		case OS_SET_OS:
-			return assign({}, state, { os: action.os });
+			return assign({}, state, action.os);
 
 		default:
 			return state;
