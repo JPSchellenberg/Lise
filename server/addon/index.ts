c38c8308a1faa75466fae19ed6086ade4bb8765b
Lipp <Lipp@miDrum.net>
2016-11-10 22:05:52 +0100

    Add flash-api to server

--- server/addon/index.ts
+++ server/addon/index.ts
@@ -1,11 +1,21 @@
 import * as os 		from 'os';
+import server		from '../core/server';
 
 import openWRT 		from './openWRT';
+import {
+	post_flash,
+	get_flash
+}		from './flash';
 
 export default function() {
 
 	// mips = arch used for arduino yun // seeeduino cloud
 	if (os.type() === 'Linux' && os.arch() === 'mips') { 
 		openWRT();
+	} else {
+		server.post('/api/v0/flash', post_flash);
+		server.get('/api/v0/flash', get_flash);
 	}
+
+	
 }
