40a7075b44d34ba67854e102616497e4d2af6178
Lipp <Lipp@miDrum.net>
2016-11-19 21:02:24 +0100

    Copy static assets via webpack

--- client/index.ts
+++ client/index.ts
@@ -1,5 +1,9 @@
 /// <reference path="../typings/index.d.ts" />
 
+require('file?name=[name].[ext]!./index.html');
+require('file?name=[name].[ext]!./assets/js/flotr2.js');
+require('file?name=[name].[ext]!./assets/js/x3dom.js');
+require('file?name=[name].[ext]!./assets/css/x3dom.css');
 import boot from './boot';
 declare var window: any;
 
