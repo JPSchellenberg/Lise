df4ab70b71406e5e8e15937cfd2105c97b152711
Lipp <Lipp@miDrum.net>
2016-12-04 21:53:20 +0100

    Remove source-files from production-build (close #47)

--- webpack.js
+++ webpack.js
@@ -8,13 +8,13 @@ module.exports = {
     './client/stylesheets/main.scss'
   ] : ['./client/index.ts', './client/stylesheets/main.scss'],
   output: {
-    path: __dirname + '/build/client',
+    path: __dirname + process.env.NODE_ENV === 'development' ? 'build/client' : 'dist/client',
     publicPath: process.env.NODE_ENV === 'development' ? 'http://localhost:8080/' : '',
     filename: "app.js",
   },
 
   // Enable sourcemaps for debugging webpack's output.
-  devtool: "source-map",
+  devtool: process.env.NODE_ENV === 'development' ? "source-map" : null,
 
   resolve: {
     // Add '.ts' and '.tsx' as resolvable extensions.
@@ -32,13 +32,13 @@ module.exports = {
       { test: /\.(woff2?|ttf|eot|svg)$/, loader: 'url?limit=10000' },
     ],
 
-    preLoaders: [
+    preLoaders: process.env.NODE_ENV === 'development' ? [
       // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-      {
+       {
         test: /\.js$/,
         loader: "source-map-loader"
-      }
-    ]
+      } 
+    ] : []
   },
   devServer: {
     contentBase: './build/client',
