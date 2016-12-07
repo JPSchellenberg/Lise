ca069b2c5d30645fb73639729c8d273bdf4578fe
Lipp <Lipp@miDrum.net>
2016-12-07 23:09:48 +0100

    Add debug to addon in server

--- server/addon/openWRT.ts
+++ server/addon/openWRT.ts
@@ -1,9 +1,18 @@
 import serialport from '../core/serialport';
 import * as libserialport from 'serialport';
 
+import * as _debug from 'debug';
+
+const debug = _debug('addon:openwrt');
+
 export default function() {
+	debug('start sequence');
+
+	debug('adding /dev/ttyATH0 as port to serialport');
 	serialport.addPort(<libserialport.portConfig>{
 		"comName": '/dev/ttyATH0'
 	});
+
+	debug('connecting to /dev/ttyATH0');
 	serialport.connect('/dev/ttyATH0');
 }
